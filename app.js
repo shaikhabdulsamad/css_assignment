c7fecd0371403bc8de55bca182896591cf50542a
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-04-10 20:32:05 +0500

    navbar updated

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function myFunction() {
     document.getElementById("myDIV").className = "show";
   }
+
+  // function myFunction() {
+  //   document.getElementById("myDIV").className = "small_screen";
+  // }
